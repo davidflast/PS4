92b3d499518d55a23592b7a50ebbadec59534ee6
davidflast <davidflasterstein@wustl.edu>
2016-03-03 01:21:29 -0600

    Successfully cleaned up the table

--- PS4.R
+++ PS4.R
@@ -1,3 +1,4 @@
+install.packages("rvest")
 library(rvest)    
 wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_elections_by_popular_vote_margin'
 
@@ -7,4 +8,20 @@ wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_ele
 temp <- wikiURL %>% 
   read_html %>%
   html_nodes("table")
+ 
+data <- html_table(temp [2], header=TRUE)
+table <- data[[1]]
+# Remove empty rows
+table <- table[-c(1,2),]
+rownames(table) <- NULL
+str(table)
+colnames(table) [3] <- "Winner"
+table [8] <- NULL
+table[6] <- NULL
+colnames(table)[7] <- "Runner Up"
+# Function to pull out names of losers and winners 
+split_names <- function(x) substring(x,(nchar(x)/2)+2 )
+# Apply functions to the data frame
+table$Winner<- sapply(table$Winner, split_names)
+table$`Runner Up` <- sapply(table$`Runner Up`, split_names)
 
